819f8cfa9a84e492760c02865b6fdf320f9ea401
bunn88 <Samantha_Morrisey@Knights.ucf.edu>
2022-09-28 23:43:22 -0400

    Animation commit

---
+++ My project/Assets/Scripts/CatController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+    
+    public AudioClip musicClipThree;
+
+    public AudioClip musicClipFour;
+
+    public AudioSource musicSource;
+    
+    Animator anim;
+    
+    void Start()
+    {
+      anim =GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+          musicSource.clip = musicClipOne;
+          musicSource.Play();
+          anim.SetInteger("State", 1);
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+         }
+         
+     if (Input.GetKeyDown(KeyCode.R))
+        {
+          musicSource.clip = musicClipTwo;
+          musicSource.Play();
+           anim.SetInteger("State", 2);
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+           anim.SetInteger("State", 0);
+         }
+
+         if (Input.GetKeyDown(KeyCode.J))
+        {
+          musicSource.clip = musicClipThree;
+          musicSource.Play();
+          anim.SetInteger("State", 4);
+         }
+
+     if (Input.GetKeyUp(KeyCode.J))
+        {
+          musicSource.Stop();
+          anim.SetInteger("State", 0);
+         }
+         
+     if (Input.GetKeyDown(KeyCode.S))
+        {
+          musicSource.clip = musicClipFour;
+          musicSource.Play();
+           anim.SetInteger("State", 3);
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+           anim.SetInteger("State", 0);
+         }
+
+    if (Input.GetKeyDown(KeyCode.L))
+        {
+          musicSource.loop = true;
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+        {
+          musicSource.loop = false;
+        }
+   }
+} 
